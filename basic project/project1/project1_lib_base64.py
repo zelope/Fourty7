44fd6170d9b0e20076348b8c1c08cb30b66be3c7
zelope <ljg980214@gmail.com>
2024-02-07 00:58:24 +0900

    modified:   basic project/project1/01. project1_outline.ipynb 	new file:   basic project/project1/02-0. project1_lib.ipynb 	new file:   basic project/project1/img/0005303739_001_20220828154501895.jpg 	new file:   basic project/project1/img/base64_example.png 	new file:   basic project/project1/img/binary_icon.png 	new file:   basic project/project1/img/decoded.jpg 	new file:   basic project/project1/img/decoded.png 	new file:   basic project/project1/img/gensim_logo.png 	new file:   basic project/project1/img/word2vec.png 	new file:   basic project/project1/img/word2vec_exp.png 	new file:   basic project/project1/project1_lib_base64.py 	new file:   basic project/project1/result_img/decode.png

---
+++ basic project/project1/project1_lib_base64.py
@@ -0,0 +1,26 @@
+import os
+
+# 현재 스크립트 파일의 디렉토리 경로
+script_dir = os.path.dirname(__file__)
+print(script_dir)
+
+import base64
+
+file_name = "0005303739_001_20220828154501895.jpg"
+path = os.path.join(script_dir, "img", file_name)
+# 바이너리 파일 읽기
+with open(path, 'rb') as img:
+    image = img.read()
+
+# base64 인코딩
+with open(path, 'rb') as img:
+    data = img.read()
+    encoded = base64.b64encode(data)
+
+# base64 디코딩
+decoded = base64.decodebytes(encoded)
+
+file_path = os.path.join(script_dir, "result_img", "decode.png")
+
+with open(file_path, 'wb') as image_file:
+    image_file.write(decoded)
